83c360b715a388433349aa1650dd4a1d1a37d0aa
Doriel aboya <doriel494@gmail.com>
2020-07-08 13:56:14 +0300

    Updates

---
+++ precache-manifest.bce78cad1d590769f37204f4c7276a73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c480d15ef1d9a051fd4002815651766b",
+    "url": "/cards-game-war/index.html"
+  },
+  {
+    "revision": "27dfbd3ce6bbf3662b99",
+    "url": "/cards-game-war/static/css/2.8c28660b.chunk.css"
+  },
+  {
+    "revision": "27a0131bbec41cb1321f",
+    "url": "/cards-game-war/static/css/main.87a95b53.chunk.css"
+  },
+  {
+    "revision": "27dfbd3ce6bbf3662b99",
+    "url": "/cards-game-war/static/js/2.8bc8a360.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cards-game-war/static/js/2.8bc8a360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a0131bbec41cb1321f",
+    "url": "/cards-game-war/static/js/main.058eca7a.chunk.js"
+  },
+  {
+    "revision": "e38f88c3e3433fc511ad",
+    "url": "/cards-game-war/static/js/runtime-main.cecb9240.js"
+  }
+]);
